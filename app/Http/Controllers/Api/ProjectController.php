a5627e534f78ee333c50d9b8eed179ba2a6f98df
LucaLF-dev <139238422+LucaLF-dev@users.noreply.github.com>
2024-01-15 16:51:05 +0100

    Add - api ProjectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(Request $request)
+    {
+        // $results = Project::all();
+        $results = Project::with('category',)->paginate(20);
+
+
+        return response()->json([
+            'results' => $results,
+            'success' => true
+        ]);
+    }
+}
