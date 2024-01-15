43ae94cb0f827adf5812594485bb61c86eefd369
LucaLF-dev <139238422+LucaLF-dev@users.noreply.github.com>
2024-01-15 16:51:40 +0100

    modify - api Project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index(Request $request)
     {
         // $results = Project::all();
-        $results = Project::with('category',)->paginate(20);
+        $results = Project::with('category',)->paginate(5);
 
 
         return response()->json([
